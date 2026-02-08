f5e4a8d0365c4fbeaa38a95c1992bb0df8604a5d
masooma99 <masooe99@gmail.com>
2026-02-08 12:15:01 +0300

    init function to changed the value of lined to false and create dots

--- script.js
+++ script.js
@@ -1,59 +1,78 @@
 //caches variable
 
-const section = document.querySelector(".section")
-const dots = document.querySelectorAll(".dot")
-
+const section = document.querySelector("#div-section")
+let dotsConnection
+//const dots = document.querySelectorAll(".dot")
 //(player1 points & player2 points) ---->which will increase by 1 each time player close a box
 
+//loop to create an array of winning cases
+for (let i = 0; i < 30; i++) {
+  dotsConnection = {
+    i: {
+      line1: {
+        position: [i, i + 1],
+        lined: true,
+      },
+      line2: {
+        position: [i, i + 5],
+        lined: false,
+      },
+      line3: {
+        position: [i + 1, i + 6],
+        lined: false,
+      },
+      line4: {
+        position: [i + 5, i + 6],
+        lined: false,
+      },
+    },
+  }
+  //console.log(dotsConnection.i.line1.lined)
+} //for loop
+
 //functions
-
 //init function to recreate the dots and changed all lines to false
-
 const init = () => {
-  array.forEach((element) => {
-    //loop to add more dots into html
-  })
+  /*dotsConnection.forEach((dot, index) => {
+    dot.line1.lined = false
+    console.log(dot.line1.lined)
+    //console.log(index)
+  })*/
+  for (let i = 0; i < 30; i++) {
+    dotsConnection.i.line1.lined = false
+    //console.log(dotsConnection.i.line1.lined)
+  }
+  for (let i = 0; i < 30; i++) {
+    //for loop to create the dots
+    const newDiv = document.createElement("div")
+    newDiv.innerHTML = `<div class='dot' id=${i}></div>`
+    //console.log(newDiv)
+    //console.log(newDiv.innerHTML)
+    section.appendChild(newDiv)
+    //document.body.appendChild(newDiv)
+  }
 }
 
 //a function that will see if the selected 2 dots are able to connect or not, if it can connect it will draw a line between the 2 dots and if the box is closed the player points will increase
 
 const connectingDots = (B, C) => {
-  /**
-   * dotsConnection = {
-   *  box1:[// I will only use the first 2 lines to comparedot the 2 dots that we will line them together
-   *    line1:{
-   *        position:[A,A+1]
-   *        lined : false
-   *      }
-   *    line2:{
-   *        position:[A,A+5]
-   *        lined : false
-   *      }
-   *    line3:{
-   *        position:[A,A+6]
-   *        lined : false
-   *      }
-   *    line4:{
-   *        position:[A+5,A+6]
-   *        lined : false
-   *      }
-   *    ],
-   *  box2:[],....
-   * }
-   */
-  dotsConnection.forEach((dot, index) => {
-    let poss = dot.box[index].position[index]
+  //function#2
+
+  dotsConnection.forEach((dot) => {
+    let poss = dot.line1.position[0]
     if (B === poss) {
       if (C === B + 1) {
       } else if (C === B + 5) {
       } else {
-      } // print not valied or does not do anything
+      } // print not valid or does not do anything
     } else if (C === poss) {
       if (B === C + 1) {
       } else if (B === C + 5) {
       } else {
-      } // print not valied or does not do anything
+      } // print not valid or does not do anything
     } else {
-    } // print not valied or does not do anything
+    } // print not valid or does not do anything
   })
 }
+
+init()
